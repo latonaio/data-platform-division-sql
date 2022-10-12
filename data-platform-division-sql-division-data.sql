36b670c73069bca17c352646e72bfca0ba72c80d
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-12 23:41:11 +0900

    release

--- data-platform-division-sql-division-data.sql
+++ data-platform-division-sql-division-data.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `data_platform_division_division_data`
 (
-  `Division`              varchar(5) NOT NULL,
+  `Division`              varchar(2) NOT NULL,
     PRIMARY KEY (`Division`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
--- data-platform-division-sql-division-text-data.sql
+++ data-platform-division-sql-division-text-data.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `data_platform_division_division_text_data`
 (
-  `Division`              varchar(5) NOT NULL,
+  `Division`              varchar(2) NOT NULL,
   `Language`              varchar(3) NOT NULL,
-  `DivisionName`          varchar(5) DEFAULT NULL,
+  `DivisionName`          varchar(100) DEFAULT NULL,
     PRIMARY KEY (`Division`, `Language`),
     CONSTRAINT `DataPlatformDivisionDivisionTextData_fk` FOREIGN KEY (`Division`) REFERENCES `data_platform_division_division_data` (`Division`)
 ) ENGINE = InnoDB
